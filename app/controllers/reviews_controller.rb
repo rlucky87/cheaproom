6175cb60202f275adb59b643ae3660f405f7d37b
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2023-01-21 07:03:21 +0100

    add methodes controller reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :room_id, :text)
+  end
 end
