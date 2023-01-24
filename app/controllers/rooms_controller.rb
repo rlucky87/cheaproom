48ef1f7bd58cfaa0aa51d19e41769a4c981d63b3
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2023-01-24 21:23:55 +0100

    test fix controller id room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,13 @@
 class RoomsController < ApplicationController
 
   def index
+    @hotel = Hotel.find(params[:hotel_id])
     @rooms = Room.where(hotel_id: params[:hotel_id])
   end
 
 
   def show
+    @hotel = Hotel.find(params[:hotel_id])
     @room = Room.where(hotel_id: params[:hotel_id])
   end
 
