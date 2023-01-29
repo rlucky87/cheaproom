6eb07c1ae0bbcdfc443c938a1e9b937434c64c02
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2023-01-29 15:32:36 +0100

    define controller dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,19 @@
 class DashboardsController < ApplicationController
+
+  def show
+    @bookings = Booking.where(status: "accepted")
+    @bookings_details = []
+    @bookings.each do |booking|
+      room = Room.find(booking.room_id)
+      days = (booking.departure_date - booking.arrival_date).to_i
+      price = room.price * days
+      @bookings_details << {
+        room: room,
+        arrival_date: booking.arrival_date,
+        departure_date: booking.departure_date,
+        price: price,
+        status: booking.status
+      }
+    end
+  end
 end
